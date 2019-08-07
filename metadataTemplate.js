4efd12446cfb6a9c3168e931e12c3b824e132a29
Ariel <silerariel@gmail.com>
2019-08-07 20:34:38 +0300

    Implement reduce

--- metadataTemplate.js
+++ metadataTemplate.js
@@ -1,16 +1,14 @@
 //this file was generated using https://github.com/ArielS1/sfdc-generate-package-metadata-generator
+
 module.exports = apiVersion => {
     apiVersion = parseInt(apiVersion);
-    const apiMetadata = {};
-
-    Object.keys(allMetadata).forEach(metadataName => {
+    return Object.keys(allMetadata).reduce((acc,metadataName) => {
         const { minApiVersion, maxApiVersion, xmlName, requiredField, children = {} } = allMetadata[metadataName];
         if(apiVersion >= minApiVersion && apiVersion <= maxApiVersion) {
-            apiMetadata[metadataName] = { xmlName, requiredField, children };
+            acc[metadataName] = { xmlName, requiredField, children };
         }
-    });
-
-    return apiMetadata;
+        return acc;
+    },{});    
 }
 
 const allMetadata =
